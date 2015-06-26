92e9407f9bccbd2d401549268f50e27411464794
Mike Stratton <mike@mikestratton.net>
2015-06-26 03:04:53 -0400

    Update master_retweet.php

--- master_retweet.php
+++ master_retweet.php
@@ -2,12 +2,11 @@
 	 //variables created to pass into array
 	 $a = 'http://yourwebsite.com/yourfolder/files/intelsoftware_b.php';//twitter user account
 	 $b = 'http://yourwebsite.com/yourfolder/files/java_b.php'; //twitter user account
-	 $c = 'http://yourwebsite.com/yourfolder/files/my_quotes_b.php'; //my personal quotes
-	 $d = 'http://yourwebsite.com/yourfolder/files/nasa_technology_b.php'; //twitter user account
-	 $e = 'http://yourwebsite.com/yourfolder/files/newscientist_b.php'; //twitter user account
+	 $c = 'http://yourwebsite.com/yourfolder/files/nasa_technology_b.php'; //twitter user account
+	 $d = 'http://yourwebsite.com/yourfolder/files/newscientist_b.php'; //twitter user account
  
 	//create array of variables
-	$data = array($a,$b,$c,$d,$e,);
+	$data = array($a,$b,$c,$d,);
 	
 	//randomly choose one variable from array
 	$dkey = array_rand($data);

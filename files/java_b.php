0d0f74372d7ea457a5efb2c36d52fdf566dc55c6
Mike Stratton <mike@mikestratton.net>
2015-06-26 03:01:26 -0400

    Update java_b.php

--- files/java_b.php
+++ files/java_b.php
@@ -5,7 +5,7 @@ $t_query  = json_decode($json,true);
 
 foreach ($t_query as $key => $value) {
 	foreach ($value as $key2 => $value2){
-		if ($key2 == "text"){
+		if ($key2 == "id_str"){
 			echo $value2;
 		}
 		foreach ($value2 as $key3 => $value3){
